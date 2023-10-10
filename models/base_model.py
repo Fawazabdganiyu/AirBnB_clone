0edaccfe6ebc175e789b138c4e4cb421ef074cdf
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-10 18:22:47 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,26 @@ class BaseModel:
             an object is changed.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor
+
+        Args:
+            args (iter): A tuple of attribute for an instance.
+            kwargs (iter): A keyword arguments for an instance.
+
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == 'created_at':
+                        v = datetime.fromisoformat(v)
+                    if k == 'updated_at':
+                        v = datetime.fromisoformat(v)
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Informal string representation
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,7 +5,7 @@ Description: This module tests the class `BaseModel` in the module.
 """
 import unittest
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     """A definition of `TestBaseModel` that inherites from `TestCase`
@@ -26,9 +26,22 @@ class TestBaseModel(unittest.TestCase):
         """
         bm = BaseModel()
 
+        self.assertTrue(hasattr(bm, 'id'))
+        self.assertTrue(hasattr(bm, 'created_at'))
+        self.assertTrue(hasattr(bm, 'updated_at'))
+
         self.assertIsNotNone(bm.id)
         self.assertIsNotNone(bm.created_at)
         self.assertIsNotNone(bm.updated_at)
+
+    def test_attributes_instance(self):
+        """Test that the main attributes has the expected type.
+        """
+        bm = BaseModel()
+
+        self.assertIsInstance(bm.id, str)
+        self.assertIsInstance(bm.created_at, datetime)
+        self.assertIsInstance(bm.updated_at, datetime)
 
     def test_id_is_str(self):
         """Test that id type is `str`.
@@ -70,17 +83,6 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertNotEqual(first_update, second_update)
 
-    def test_no_update_save(self):
-        """Test that `updated_at` is constant when new attribute is added
-        """
-        bm = BaseModel()
-        first_update = bm.updated_at
-
-        bm.save()
-
-        second_update = bm.updated_at
-        self.assertNotEqual(first_update, second_update)
-
     def test_save_with_arg(self):
         """Test that TypeError is raised when `save` is called
         with any argument
@@ -96,6 +98,18 @@ class TestBaseModel(unittest.TestCase):
         bm = BaseModel()
 
         self.assertIsInstance(bm.to_dict(), dict)
+
+    def test_to_dict_keys_instances(self):
+        """Test that the keys in the dict are of expected type.
+        """
+        bm = BaseModel()
+
+        bm_dict = bm.to_dict()
+
+        self.assertIsInstance(bm_dict['id'], str)
+        self.assertIsInstance(bm_dict['created_at'], str)
+        self.assertIsInstance(bm_dict['updated_at'], str)
+        self.assertIsInstance(bm_dict['__class__'], str)
 
     def test_to_dict_attributes(self):
         """Test that the method return all the instance attribute
@@ -142,4 +156,32 @@ class TestBaseModel(unittest.TestCase):
         bm_dict = bm.to_dict()
 
         self.assertIsInstance(bm_dict['updated_at'], str)
-        
+
+    def test_with_kwargs(self):
+        """Test that the previous object is re-created
+        from a dictionary representation with the
+        expected attributes.
+        """
+        bm = BaseModel()
+        bm.name = "My_First_Model"
+        bm.number = 89
+
+        bm_dict = bm.to_dict()
+
+        new_bm = BaseModel(**bm_dict)
+
+        self.assertIsInstance(new_bm, BaseModel)
+        self.assertTrue(hasattr(new_bm, 'id'))
+        self.assertTrue(hasattr(new_bm, 'created_at'))
+        self.assertTrue(hasattr(new_bm, 'updated_at'))
+
+        self.assertEqual(new_bm.id, bm.id)
+        self.assertEqual(new_bm.created_at, bm.created_at)
+        self.assertEqual(new_bm.updated_at, bm.updated_at)
+
+        self.assertIsInstance(new_bm.id, str)
+        self.assertIsInstance(new_bm.created_at, datetime)
+        self.assertIsInstance(new_bm.updated_at, datetime)
+
+        self.assertFalse(bm is new_bm)
+        self.assertFalse(bm == new_bm)
