500cc7abc7327db1574a3c8e37336840f1d790d2
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-12 00:26:06 +0100

    Implement all method

--- console.py
+++ console.py
@@ -5,9 +5,9 @@ Description: This is a program and it contains the entry point of the
 command interpreter.
 
 """
-import cmd
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
@@ -21,7 +21,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """quits the interpreter on receiving `quit` command"""
+        """Quit the interpreter on receiving `quit` command"""
         return True
 
     def help_quit(self):
@@ -29,7 +29,7 @@ class HBNBCommand(cmd.Cmd):
         print("Quit command to exit the program\n")
 
     def do_EOF(self, line):
-        """quits the interpreter on EOF signal"""
+        """Exit the program on receiving EOF signal"""
         return True
 
     def emptyline(self):
@@ -39,6 +39,9 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """Creates a new instance of BaseModel,
         saves it (to the JSON file) and prints the id
+
+        Example:
+            `(hbnb) create BaseModel`
 
         """
         args = line.split()
@@ -56,8 +59,13 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representation of an instance
         based on the class name and `id`.
 
+        Example:
+            `(hbnb) show BaseModel 1234-1234-1234`
+
         """
         args = line.split()
+
+        # Get objects dictionary in the form { <BaseModel.id>=<obj>,... }
         all_objs = storage.all()
 
         if len(args) == 0:
@@ -70,6 +78,51 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
         else:
             print(all_objs[f"BaseModel.{args[1]}"])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and
+        `id`. Then save the change into the JSON file
+
+        Example:
+            `(hbnb) destroy BaseModel 1234-1234-1234`
+
+        """
+        args = line.split()
+
+        # Get objects dictionary in the form { <BaseModel.id>=<obj>,... }
+        all_objs = storage.all()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"BaseModel.{args[1]}" not in all_objs.keys():
+            print("** no instance found **")
+        else:
+            del all_objs[f"BaseModel.{args[1]}"]
+            storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances,
+        based or not on the class name
+
+        Example:
+           `(hbnb) all BaseModel` or
+           `(hbnb) all`
+
+        """
+        args = line.split()
+        # Get objects dictionary in the form { <BaseModel.id>=<obj>,... }
+        all_objs = storage.all()
+
+        if len(args) == 0 or (len(args) == 1 and args[0] == "BaseModel"):
+            # Represent all instances in a list
+            all_objs_list = [str(v) for v in all_objs.values()]
+            print(all_objs_list)
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
