f3567d793a32fd1228a0225cf8beb3f59440f692
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-11 19:02:59 +0100

    Implement `show`.

--- console.py
+++ console.py
@@ -6,13 +6,28 @@ command interpreter.
 
 """
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """A definition of `HBNBCommand` class inherited from `Cmd`
     for command interpreter
+
+    Args:
+        line (string): A space separated argument for the program.
+
     """
     prompt = "(hbnb) "
+
+    def precmd(self, line):
+        """Overrides to separate the `line` argument based on it spaces
+
+        Return (list):
+            A list of arguments passed to the program
+
+        """
+        args = line.split(' ')
+
+        return cmd.Cmd.precmd(self, args)
 
     def do_quit(self, line):
         """quits the interpreter on receiving `quit` command"""
@@ -29,6 +44,52 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Overide the base class method not to print anything"""
         pass
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+
+        """
+        if len(line) == 0:
+            print("** class name missing **")
+        if line[0] != "BaseModel":
+            print("** class doesn't exist **")
+
+            # Create the expected instance
+            obj = BaseModel()
+            obj.save()
+            print(f"{obj.id}")
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and `id`.
+
+        """
+        if len(line) == 0:
+            print("** class name missing **")
+        if line[0] != "BaseModel":
+            print("** class doesn't exist **")
+        if line[1] == None:
+            print("** instance id missing **")
+
+        search = f"BaseMode.{line[1]}"
+        found = 0
+        try:
+            with open("file.json", mode='r', encoding='utf-8') as f:
+                objs_dict = json.load(f)
+
+            # Search the dict generated from file.json
+            for k in objs_dict.keys():
+                if k == search:
+                    found = 1
+                    obj_attr = objs_dict[k]
+                    obj = BaseModel(**objs_attr)
+                    print(obj)
+                else:
+                    found = 0
+
+            if found = 0:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
