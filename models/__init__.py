66f7c3e34a9d400c2b5b6da9cd45e5bcbf92b5e8
Vulcanric <jneric49@gmail.com>
2023-10-12 02:20:49 +0100

    merge files models/__init__.py and models/base_model in the remote branch to local branch

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from models.engine import file_storage
+
+# Create a FileStorage instance
+storage = file_storage.FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ This module defines a `BaseModel` class
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -40,6 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Informal string representation
@@ -51,11 +53,11 @@ class BaseModel:
         return f"[BaseModel] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the public instance attribute `updated_at` with the
-        current datetime.
-
+        """Updates objects and the public instance attribute `updated_at`
+        with the current datetime of updation.
         """
         self.updated_at = datetime.now()
+        storage.save()  # Save the updated objects
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the __dict__
