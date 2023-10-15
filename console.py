9d53cfa68c47738cb59b6511aac3495a31d6703e
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-15 02:35:11 +0100

    Import `Review` class in console

--- console.py
+++ console.py
@@ -9,12 +9,13 @@ import cmd
 import re
 import shlex
 from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
