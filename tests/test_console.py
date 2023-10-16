fca6bc9ca78302af00b6e8097a30c7c75a3c283c
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-16 05:54:49 +0100

    Test help

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,3 +3,19 @@
 Module Name: tests/test_console.py
 Description: This module test the interpreter functionallity.
 """
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """
+    Test console
+    """
+    def test_help(self):
+        """
+        Test that help is working
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
